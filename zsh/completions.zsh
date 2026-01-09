5f1c5807c321c70902203e60013496b5a349b44d
Bartu OZEL <info@bartuozel.com>
2026-01-09 11:44:10 +0300

    Add opencode completions and Tailscale alias

---
+++ zsh/completions.zsh
@@ -0,0 +1,16 @@
+# Zsh completions configuration
+
+# opencode completions
+_opencode_yargs_completions() {
+  local reply
+  local si=$IFS
+  IFS=$'\n'
+  reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" opencode --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  if [[ ${#reply} -gt 0 ]]; then
+    _describe 'values' reply
+  else
+    _default
+  fi
+}
+compdef _opencode_yargs_completions opencode
